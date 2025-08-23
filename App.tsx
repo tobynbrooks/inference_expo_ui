2abfb51d3fdaffbff890d10d4d47a0893e25cf92
Tobyn Brooks <tobyn.brooks.emba-j22@said.oxford.edu>
2025-08-23 21:09:09 +0100

    initial commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>upload photo, for a snappy service</Text>
       <StatusBar style="auto" />
     </View>
   );
